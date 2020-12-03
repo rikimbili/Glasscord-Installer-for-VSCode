e3d1ff8b9dd7a734a883eb3d0e56fa19f4921de3
Raciel <39206666+Rikimbili@users.noreply.github.com>
2020-12-03 05:51:03 -0500

    Restructuring and some optimizations

--- glasscord_installer.c
+++ glasscord_installer.c
@@ -18,44 +18,81 @@
 #define BUFFER_SIZE 8192
 #define PATH_SIZE 256
 
+int vscodeInstall(void);
+int menu(void);
 void terminate(char *msgStr, int exitCode);
 void spinner(unsigned int time_sec);
-void printTitle(void);
 
-int main()
+int main(void)
 {
-    FILE *ifp, *ofp, *gcordfp, *o_gcordfp, *file_check;
-    char PATH[PATH_SIZE], gcordPATH[PATH_SIZE];
-    char currentBUFF[BUFFER_SIZE];
-    const char searchSTR[] = {"\"main\":"}, asarSTR[] = {"  \"main\": \"./glasscord.asar\",\n"}, *dir_check;
+    int main_return; 
+    /* |===========================|
+          Title and Menu system 
+    */
+    printf("\t  ______ _                                     _    _____                      _ _             \n");
+    printf("\t / _____) |                                   | |  (_   _)           _        | | |            \n");
+    printf("\t| /  ___| | ____  ___  ___  ____ ___   ____ _ | |    | |  ____   ___| |_  ____| | | ____  ____ \n");
+    printf("\t| | (___) |/ _  |/___)/___)/ ___) _ \\ / ___) || |    | | |  _ \\ /___)  _)/ _  | | |/ _  )/ ___)\n");
+    printf("\t| \\____/| ( ( | |___ |___ ( (__| |_| | |  ( (_| |   _| |_| | | |___ | |_( ( | | | ( (/ /| |    \n");
+    printf("\t \\_____/|_|\\_||_(___/(___/ \\____)___/|_|   \\____|  (_____)_| |_(___/ \\___)_||_|_|_|\\____)_|    \n\n");
+    printf("\nSelect a program for Glasscord Installer to check and install required components:\n");
 
-    printTitle();
-    
+    while(1)
+    {
+        switch(menu())
+        {
+            case 0:
+                exit(0);
+                break;
+            case 1:
+                if(vscodeInstall())
+                {
+                    printf("\n-> Glasscord for VS Code installed successfully! <-\n");
+                }
+            break;
+        }
+        printf("--------------------------------------------------------------\n");
+        printf("Do you wish to install Glasscord to any other program or exit? \n");
+    }
+
+    return 0;
+}
+
+
+//  +-+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  Perform all required operations to install glasscord for VS Code, 1 returned if successful
+int vscodeInstall(void)
+{
+    FILE *file_check;
+    char currentBUFF[BUFFER_SIZE];
+    char PATH[PATH_SIZE], executablePATH[PATH_SIZE],gcordPATH[PATH_SIZE];
+    const char searchSTR[] = {"\"main\":"}, asarSTR[] = {"  \"main\": \"./glasscord.asar\",\n"};
+
     // Store current dir containing glasscord.asar to copy it later
-    strcpy(gcordPATH, _getcwd(NULL, 0));
+    strcpy(executablePATH, _getcwd(NULL, 0));
+    strcpy(gcordPATH, executablePATH);
     strcat(gcordPATH, "\\resources\\glasscord.asar");
+
     // Get dir of VS CODE
     strcpy(PATH, getenv("LOCALAPPDATA"));
     strcat(PATH, "\\Programs\\Microsoft VS Code\\resources\\app");
 
     // Check if VS CODE directory exists, else exit
-    dir_check = PATH;
     struct stat stat_buff;
-    if (stat(dir_check, &stat_buff) != 0 && !S_ISDIR(stat_buff.st_mode))
+    if (stat(PATH, &stat_buff) != 0 && !S_ISDIR(stat_buff.st_mode))
     {
-        terminate("Visual Studio code directory could not be found\nPLease check your installation and retry.", 1);
+        terminate("Visual Studio code directory could not be found\nPLease check your installation and retry.", -1);
+        return 0;
     }
-
-    // Change working dir
-    if (_chdir(PATH) != 0)
+    // Change working dir to Microsoft VS Code/resources/app
+        if (_chdir(PATH) != 0)
     {
-        terminate("Working directory could not be changed.", 1);
+        terminate("Unable to change working directory.", -1);
+        return 0;
     }
-
-    /* |=====================================================================|
-                    Start of file operations in VS CODE dir */
 
     // Opening glasscord.asar files for read/writing, If glasscord.asar not found, copy it from gcordPATH
+    FILE *gcordfp, *o_gcordfp;
     gcordfp = fopen(gcordPATH, "rb");
     o_gcordfp = fopen("glasscord.asar", "r");
     if (o_gcordfp == NULL)
@@ -63,11 +100,13 @@ int main()
         fclose(o_gcordfp);
         if ((o_gcordfp = fopen("glasscord.asar", "wb")) == NULL)
         {
-            terminate("glasscord.asar file could not be created in the Visual Studio Code directory. Please close VS Code and retry.", 1);
+            terminate("glasscord.asar file could not be created in the Visual Studio Code directory. Please close VS Code and retry.", -1);
+            return 0;
         }
         if (gcordfp == NULL)
         {
-            terminate("glasscord.asar not found in the executable's directory.", 1);
+            terminate("glasscord.asar not found in the executable's directory.", -1);
+            return 0;
         }
 
         size_t bytes;
@@ -75,19 +114,21 @@ int main()
         {
             if (fwrite(currentBUFF, 1, bytes, o_gcordfp) != bytes)
             {
-                terminate("glasscord.asar could not be copied.", 1);
+                terminate("glasscord.asar could not be copied.", -1);
+                return 0;
             }
         }
     }
     else
     {
-        printf("glasscord.asar already present in VS Code directory.\n");
+        // printf("glasscord.asar already present in VS Code directory.\n");
     }
     fclose(gcordfp);
     fclose(o_gcordfp);
 
     // Check if file package.original.json and package.json already exist. If package.json exists as well, check if
     // it has the required modification already, if not then just rename it and proceed
+    int is_package_mod = 0;
     if ((file_check = fopen("package.original.json", "r")) != NULL)
     {
         fclose(file_check);
@@ -97,68 +138,113 @@ int main()
             fgets(currentBUFF, BUFFER_SIZE, file_check);
             if (strstr(currentBUFF, "glasscord.asar") != NULL)
             {
-                terminate("Required modifications already present in package.json file.", 0);
+                // printf("Required modifications already present in package.json file.\n");
+                is_package_mod = 1;
             }
         }
         fclose(file_check);
-        if (remove("package.original.json") == -1)
+        if (!is_package_mod && remove("package.original.json") == -1)
         {
-            terminate("Unable to remove the existing package.original.json file.", 1);
+            terminate("Unable to remove the existing package.original.json file.", -1);
+            return 0;
         }
     }
     else if ((file_check = fopen("package.json", "r")) == NULL)
     {
-        terminate("Cannot open package.json. Either the file doesn't exist or you don't have the right privilieges.", 1);
+        terminate("Cannot open package.json. Either the file doesn't exist or you don't have the right privilieges.", -1);
+        return 0;
     }
     fclose(file_check);
-    if (rename("package.json", "package.original.json") == -1)
+    if (!is_package_mod && rename("package.json", "package.original.json") == -1)
     {
-        terminate("Unable to rename package.json to package.original.json.", 1);
+        terminate("Unable to rename package.json to package.original.json.", -1);
+        return 0;
     }
 
-    // Copy data from package.original.json to package.json and modifies package.json
-    ifp = fopen("package.original.json", "r");
-    ofp = fopen("package.json", "w+");
-    if (ifp == NULL || ofp == NULL)
+
+    if(!is_package_mod)
     {
-        terminate("Cannot open/create package.original.json/package.json file respectively.", 1);
-    }
-    int is_mod = 0;
-    while (!feof(ifp))
-    {
-        fgets(currentBUFF, BUFFER_SIZE, ifp);
-        if (is_mod == 0 && strstr(currentBUFF, searchSTR) != NULL)
+        // Copy data from package.original.json to package.json and modifies package.json
+        FILE *ifp, *ofp;
+        ifp = fopen("package.original.json", "r");
+        ofp = fopen("package.json", "w+");
+        if (ifp == NULL || ofp == NULL)
         {
-            fputs(asarSTR, ofp);
-            is_mod = 1;
+            terminate("A file cannot be opened in VS Code directory. Please close VS Code and retry.", -1);
+            return 0;
         }
-        else
+        int is_mod = 0;
+        while (!feof(ifp))
         {
-            fputs(currentBUFF, ofp);
+            fgets(currentBUFF, BUFFER_SIZE, ifp);
+            if (is_mod == 0 && strstr(currentBUFF, searchSTR) != NULL)
+            {
+                fputs(asarSTR, ofp);
+                is_mod = 1;
+            }
+            else
+            {
+                fputs(currentBUFF, ofp);
+            }
         }
+        fclose(ifp);
+        fclose(ofp);
     }
-    fclose(ifp);
-    fclose(ofp);
     /*                End of file operations in VS CODE dir
-      |=====================================================================| */
+      |=====================================================================| 
+    */
 
-    terminate(NULL, 0);
-    return 0;
+   // Change dir back to executable's path
+    if (_chdir(executablePATH) != 0)
+    {
+        terminate("Unable to return to the executable's directory", 1);
+    }
+    return 1;
 }
 
-//  +-+-+-+-+-+-+-+-+-+-+-+-
-//  Display message and exit
-//  +-+-+-+-+-+-+-+-+-+-+-+-
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  Prints the home screen's specified module. 0 for title and 1 for option menu
+int menu(void)
+{
+    int user_choice, counter = 0;
+      
+    printf("\n1. Visual Studio Code\n0. Exit\n\nYour choice: ");
+    scanf("%d", &user_choice);
+    while(user_choice != 0 && user_choice != 1)
+    {
+        if(counter <= 3)
+            printf("Please enter a correct choice number: ");
+        else if (counter == 4)
+            printf("We can keep doing this forever if you want. Please try again: ");
+        else if (counter == 5)
+            printf("There are only two choices, I'm still wondering where you got %d from. Try again: ", user_choice);
+        else if (counter < 10)
+            printf("Okay, I'm starting to think your cat sat on your keyboard or something: ");
+        else
+        {
+            printf("Alright, I'm gonna do you a favor and press 0 for you...");
+            spinner(3);
+            exit(0);
+        }
+        scanf("%d", &user_choice);
+        counter++;
+    }
+    return user_choice; 
+
+}
+
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  Display message and exit the program, -1 exitCode used when a program termination is not desired
 void terminate(char *msgStr, int exitCode)
 {
-    if(exitCode != 0 && msgStr)
+    if(exitCode && msgStr)
     {
         printf("Error: %s", msgStr);
         spinner(3);
     }
     else if(exitCode == 0 && msgStr == NULL)
     {
-        printf("Glasscord was successfully installed\n");
+        printf("No errors reported during ins\n");
     }
     else if(exitCode == 0)
     {
@@ -169,14 +255,22 @@ void terminate(char *msgStr, int exitCode)
         printf("An error ocurred but no information was provided");
         spinner(3);
     }
-    printf("Press ENTER to exit...");
-    getchar();
-    exit(exitCode);
+
+    if(exitCode == -1)
+    {
+        printf("Press ENTER to continue...");
+        getchar();
+    }
+    else
+    {
+        printf("Press ENTER to exit...");
+        getchar();
+        exit(exitCode);
+    }
 }
 
 //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //  Spinner animation w/ added delay in seconds
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 void spinner(unsigned int time_sec)
 {
     int counter, i = 0;
@@ -194,14 +288,4 @@ void spinner(unsigned int time_sec)
     }
     printf("\b ");
     printf("\n");
-}
-
-void printTitle(void)
-{
-    printf("\t  ______ _                                     _    _____                      _ _             \n");
-    printf("\t / _____) |                                   | |  (_   _)           _        | | |            \n");
-    printf("\t| /  ___| | ____  ___  ___  ____ ___   ____ _ | |    | |  ____   ___| |_  ____| | | ____  ____ \n");
-    printf("\t| | (___) |/ _  |/___)/___)/ ___) _ \\ / ___) || |    | | |  _ \\ /___)  _)/ _  | | |/ _  )/ ___)\n");
-    printf("\t| \\____/| ( ( | |___ |___ ( (__| |_| | |  ( (_| |   _| |_| | | |___ | |_( ( | | | ( (/ /| |    \n");
-    printf("\t \\_____/|_|\\_||_(___/(___/ \\____)___/|_|   \\____|  (_____)_| |_(___/ \\___)_||_|_|_|\\____)_|    \n\n");
 }
